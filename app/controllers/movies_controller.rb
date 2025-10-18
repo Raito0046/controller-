bff9f0b2ab499791f12e441ee7ef4b7b9109bf20
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2025-10-18 07:10:21 +0000

    Create MoviesController with full RESTful actions and comments

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,74 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[show edit update destroy]
+
+  # GET /movies
+  # HTTP Verb: GET
+  # URL: /movies
+  def index
+    @movies = Movie.order(created_at: :desc)
+  end
+
+  # GET /movies/:id
+  # HTTP Verb: GET
+  # URL: /movies/:id
+  def show
+    # @movie is set by set_movie
+  end
+
+  # GET /movies/new
+  # HTTP Verb: GET
+  # URL: /movies/new
+  def new
+    @movie = Movie.new
+  end
+
+  # POST /movies
+  # HTTP Verb: POST
+  # URL: /movies
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie was successfully created."
+    else
+      flash.now[:alert] = "There were errors creating the movie."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /movies/:id/edit
+  # HTTP Verb: GET
+  # URL: /movies/:id/edit
+  def edit
+    # @movie is set by set_movie
+  end
+
+  # PATCH/PUT /movies/:id
+  # HTTP Verb: PATCH or PUT
+  # URL: /movies/:id
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie was successfully updated."
+    else
+      flash.now[:alert] = "There were errors updating the movie."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /movies/:id
+  # HTTP Verb: DELETE
+  # URL: /movies/:id
+  def destroy
+    @movie.destroy
+    redirect_to movies_path, notice: "Movie was successfully deleted."
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :director, :release_year, :rating)
+  end
+end
