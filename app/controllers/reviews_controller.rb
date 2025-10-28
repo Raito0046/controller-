56638b1f64890d817eb658b5915f45e87d212f8c
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2025-10-28 09:05:53 +0000

    reviews: add index action to ReviewsController

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+  before_action :set_movie
+
+  # GET /movies/:movie_id/reviews
+  # HTTP Verb: GET
+  # URL: /movies/:movie_id/reviews
+  def index
+    @reviews = @movie.reviews.order(created_at: :desc)
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+end
