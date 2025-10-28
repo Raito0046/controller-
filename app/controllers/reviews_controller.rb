c1886b32db9cdf82573c83d60aea3388fdcfa096
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2025-10-28 09:06:41 +0000

    reviews: add new action and form partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,13 @@ class ReviewsController < ApplicationController
     @reviews = @movie.reviews.order(created_at: :desc)
   end
 
+  # GET /movies/:movie_id/reviews/new
+  # HTTP Verb: GET
+  # URL: /movies/:movie_id/reviews/new
+  def new
+    @review = @movie.reviews.build
+  end
+
   private
 
   def set_movie
