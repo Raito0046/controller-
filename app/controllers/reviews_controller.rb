f693333cd93a9f29943617a21352a1c090b93f4e
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2025-10-28 09:06:54 +0000

    reviews: add create action with validation handling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,9 +15,26 @@ class ReviewsController < ApplicationController
     @review = @movie.reviews.build
   end
 
+  # POST /movies/:movie_id/reviews
+  # HTTP Verb: POST
+  # URL: /movies/:movie_id/reviews
+  def create
+    @review = @movie.reviews.build(review_params)
+    if @review.save
+      redirect_to movie_review_path(@movie, @review), notice: "Review was successfully created."
+    else
+      flash.now[:alert] = "Please correct the errors below."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_movie
     @movie = Movie.find(params[:movie_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:reviewer, :rating, :body)
   end
 end
