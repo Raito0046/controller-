d0a350c82b88a2b42057e4be0f150941bc87af37
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2025-11-18 08:59:36 +0000

    Add omniauth callback routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# config/routes.rb (add inside Rails.application.routes.draw do ... end)
+get "/auth/:provider/callback", to: "sessions#omniauth"
+get "/auth/failure", to: "sessions#failure"
+
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :movies
   root "movies#index"
 end
+
+
