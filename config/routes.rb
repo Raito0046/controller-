032c681cf5aa210c78e697c940725645dd9d20cf
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2025-11-25 09:26:11 +0000

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
-# config/routes.rb (add inside Rails.application.routes.draw do ... end)
-get "/auth/:provider/callback", to: "sessions#omniauth"
-get "/auth/failure", to: "sessions#failure"
-
 Rails.application.routes.draw do
+  # OmniAuth callbacks
+  get "/auth/:provider/callback", to: "sessions#omniauth"
+  get "/auth/failure", to: "sessions#failure"
+
   resource :session
   resources :passwords, param: :token
   resources :movies
   resources :posters
+
   root "movies#index"
 end
-
-
